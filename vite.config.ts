6d1439403b23fd6c3d2c4c0fe1630653c7696529
unknown <727724eucs182@skcet.ac.in>
2025-06-05 17:04:53 +0530

    fix: update script source and build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,10 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
 });
